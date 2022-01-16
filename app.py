33125201bb0bbb994bf83b0bf35fb149dc3481e9
Gareth Cairns <garethcairns97@gmail.com>
2022-01-16 14:24:36 +0000

    Edited to return a sample webpage.

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from os import listdir
+import sys
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Run build.sh to delete old images, delete kubernetes resources, restart."
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
